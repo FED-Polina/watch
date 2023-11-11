c242f491b992f5baa5e9df1d495e45b6f60b5da1
user <user@users-iMac-3.local>
2023-11-11 09:37:53 +0300

    Commit

---
+++ myapp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  myapp WatchKit Extension
+//
+//  Created by user on 11.11.2023.
+//  Copyright © 2023 user. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
